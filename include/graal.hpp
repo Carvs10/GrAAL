22b8d93939a0cc14fd20a0ef4dfbad69e4dc7163
Victor Vieira <victorvieira89@gmail.com>
2019-04-19 17:18:58 -0300

    First solution finished

---
+++ include/graal.hpp
@@ -0,0 +1,44 @@
+#ifndef GRAAL_LIB_H
+#define GRAAL_LIB_H
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using byte = unsigned char;
+using Compare = bool (*)(const void *, const void *);
+using Predicate = bool (*)(const void *);
+using Equal = bool (*)(const void *, const void *);
+
+namespace graal
+{
+const void *min(const void *first, const void *last, size_t size, Compare cmp);
+
+void reverse(void *first, void *last, size_t size);
+
+void *copy(const void *first, const void *last, const void *d_first, size_t size);
+
+void *clone(const void *first, const void *last, size_t size);
+
+const void *find_if(const void *first, const void *last, size_t size, Predicate p);
+
+const void *find(const void *first, const void *last, size_t size, const void *value, Equal eq);
+
+bool all_of(const void *first, const void *last, size_t size, Predicate p);
+
+bool any_of(const void *first, const void *last, size_t size, Predicate p);
+
+bool none_of(const void *first, const void *last, size_t size, Predicate p);
+
+bool equal(const void *first1, const void *last1, const void *first2, size_t size, Equal eq);
+
+bool equal(const void *first1, const void *last1, const void *first2, const void *last2, size_t size, Equal eq);
+
+void *unique(void *first, void *last, size_t size, Equal eq);
+
+void *partition(void *first, void *last, size_t size, Predicate p);
+
+void qsort(void *ptr, std::size_t count, size_t size, Equal eq);
+} // namespace graal
+
+#endif
---
+++ src/graal.cpp
@@ -0,0 +1,86 @@
+#include "../include/graal.hpp"
+
+namespace graal
+{
+const void *min(const void *first, const void *last, size_t size, Compare cmp)
+{
+  const byte *first_iterator = (const byte *)first;
+  const void *smaller = first_iterator;
+
+  while (first_iterator != last)
+  {
+    if (cmp(first_iterator, smaller))
+    {
+      smaller = first_iterator;
+    }
+    first_iterator += size;
+  }
+
+  return smaller;
+}
+
+void reverse(void *first, void *last, size_t size)
+{
+  byte *first_iterator = (byte *)first;
+  byte *last_iterator = (byte *)last;
+
+  while (first_iterator < last_iterator)
+  {
+    first_iterator += size;
+    last_iterator -= size;
+  }
+}
+
+void *copy(const void *first, const void *last, const void *d_first, size_t size)
+{
+}
+
+void *clone(const void *first, const void *last, size_t size)
+{
+}
+
+const void *find_if(const void *first, const void *last, size_t size, Predicate p)
+{
+}
+
+const void *find(const void *first, const void *last, size_t size, const void *value, Equal eq)
+{
+}
+
+bool all_of(const void *first, const void *last, size_t size, Predicate p)
+{
+  return false;
+}
+
+bool any_of(const void *first, const void *last, size_t size, Predicate p)
+{
+  return false;
+}
+
+bool none_of(const void *first, const void *last, size_t size, Predicate p)
+{
+  return false;
+}
+
+bool equal(const void *first1, const void *last1, const void *first2, size_t size, Equal eq)
+{
+  return false;
+}
+
+bool equal(const void *first1, const void *last1, const void *first2, const void *last2, size_t size, Equal eq)
+{
+  return false;
+}
+
+void *unique(void *first, void *last, size_t size, Equal eq)
+{
+}
+
+void *partition(void *first, void *last, size_t size, Predicate p)
+{
+}
+
+void qsort(void *ptr, std::size_t count, size_t size, Equal eq)
+{
+}
+} // namespace graal
